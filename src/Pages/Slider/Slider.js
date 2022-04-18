8a7adf2ea36d95e3a30bbd2d349a86b20ed5b223
Your Name <you@example.com>
2022-04-18 08:07:02 +0600

    banner added

---
+++ src/Pages/Slider/Slider.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import slider1 from '../../Images/slider1.jpg'
+import slider2 from '../../Images/slider2.jpg'
+import slider3 from '../../Images/slider3.jpg'
+
+const Slider = () => {
+    return (
+        <div>
+            <Carousel fade>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption >
+                        <h3 >LEGIT WEDDING PHOTOS</h3>
+                        <p>You've been to weddings - but probably not on the planning/getting married
+
+                            side of things.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3 >LEGIT WEDDING PHOTOS</h3>
+                        <p>You've been to weddings - but probably not on the planning/getting married
+
+                            side of things.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3 >LEGIT WEDDING PHOTOS</h3>
+                        <p>You've been to weddings - but probably not on the planning/getting married
+
+                            side of things.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel >
+        </div >
+    );
+};
+
+export default Slider;
