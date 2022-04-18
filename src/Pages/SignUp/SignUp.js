ed6bd9e53093b3907d932cdffb9bfe9cd5553141
Your Name <you@example.com>
2022-04-18 14:22:26 +0600

    signup completed

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,45 +1,72 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
 
     const navigate = useNavigate();
-    const nameRef = useRef('');
+    const confirmPasswordRef = useRef('');
     const emailRef = useRef('');
     const passwordRef = useRef('');
+    const [errorr, setError] = useState('');
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate('/');
+    }
+
+    if (loading) {
+        return <div class="spinner-border" style={{ marginTop: "150px" }} role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    }
+
     const handleSubmit = event => {
         event.preventDefault();
-        const name = nameRef.current.value;
+
         const email = emailRef.current.value;
         const password = passwordRef.current.value
-        console.log(email, password, name);
+        const confirmPassword = confirmPasswordRef.current.value;
+        if (password !== confirmPassword) {
+            setError('Your two password did not match');
+            return;
+        }
+        if (password.length < 6) {
+            setError('Password must be 6 characters or longer');
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password);
+
     }
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='fw-bold mt-5'>Please Sign Up</h2>
             <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
-                    <Form.Label >Name</Form.Label>
-                    <Form.Control ref={nameRef} type="text" placeholder="Enter name" required />
-
-                </Form.Group>
 
                 <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
                     <Form.Label >Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+
                 </Form.Group>
 
                 <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3 text-start" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+                <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control ref={confirmPasswordRef} type="password" placeholder="Confirm Password" required />
                 </Form.Group>
+                <p className='text-danger text-start'>{errorr}</p>
                 <Button className='px-5 mb-3' variant="dark" type="submit">
                     Submit
                 </Button>
